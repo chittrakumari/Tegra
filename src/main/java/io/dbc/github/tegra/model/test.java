777348ede6801b1156c515ac868d84d8566cb27c
Chittra Kumari <chittra.kumari_cs.ccv19@gla.ac.in>
2022-02-14 19:31:59 +0530

    initial commit

---
+++ src/main/java/io/dbc/github/tegra/model/test.java
@@ -0,0 +1,15 @@
+/**
+ * User: Chittra Kumari
+ * Date: 14-02-2022
+ * IDE: IntelliJ IDEA
+ * File: io.dbc.github.tegra.model.test.java
+ * JDK: Oracle JDK 17
+ */
+
+package io.dbc.github.tegra.model;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
