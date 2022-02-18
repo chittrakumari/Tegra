f381a372be868930573517967ca0d0f5096118de
dbc2201 <divyanshb22@gmail.com>
2022-02-18 21:47:38 +0530

    added library for log4j

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module io.dbc.github.tegra {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.apache.logging.log4j;
 
     opens io.dbc.github.tegra to javafx.fxml;
     exports io.dbc.github.tegra;
